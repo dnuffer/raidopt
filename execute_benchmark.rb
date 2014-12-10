2b002c1651e2e74507712c93006e1f1ffd209766
Dan Nuffer <danielnuffer@gmail.com>
2014-12-09 21:25:20 -0700

    printing times and flushing STDOUT.

--- execute_benchmark.rb
+++ execute_benchmark.rb
@@ -2,6 +2,7 @@ require 'rubygems'
 require 'rbvmomi'
 require './vm_utils'
 require 'highline/import'
+require 'date'
 
 if ARGV.size != 68
   STDERR.puts "Invalid args: template 1-8 0|1|5|6|10|50|60 64|128|256|512|1024 normal|ahead write-back|write-thru cached|direct swap-size disk-size memory-size num-cpus scheduler block-size ext4-stride ext4-stripe-width ext4-journal-mode ext4-barrier ext4-atime ext4-diratime ext4-64-bit ext4-dir-index ext4-dir-nlink, ext4-extent ext4-extra-isize ext-ext-attr ext4-filetype ext4-flex-bg ext4-flex-bg-num-groups ext4-huge-file ext4-sparse-super2 ext4-mmp ext4-resize-inode ext4-sparse-super ext4-uninit-bg ext4-inode-size ext4-inode-ratio ext4-num-backup-sb ext4-packed-meta-blocks ext4-acl ext4-inode-allocator ext4-user-xattr ext4-journal-commit-interval ext4-journal-checksum-async-commit ext4-delalloc ext4-max-batch-time ext4-min-batch-time ext4-journal-ioprio ext4-auto-da-alloc ext4-discard ext4-dioread-lock ext4-i-version kernel-vm-dirty-ratio kernel-vm-dirty-background-ratio kernel-vm-swappiness kernel-read-ahead kernel-fs-read-ahead kernel-dev-ncq ext4-bh kernel-vm-vfs-cache-pressure kernel-vm-dirty-expire-centisecs kernel-vm-dirty-writeback-centisecs kernel-vm-extfrag-threshold kernel-vm-hugepages-treat-as-movable kernel-vm-laptop-mode kernel-vm-overcommit-memory kernel-vm-overcommit-ratio kernel-vm-percpu-pagelist-fraction kernel-vm-zone-reclaim-mode"
@@ -208,6 +209,7 @@ puts "setting ncq on device"
 run.call("echo #{kernel_dev_ncq} > /sys/block/sdc/device/queue_depth")
 
 puts "formatting test disk"
+STDOUT.flush
 ext4_options = [
   ext4_dir_index, 
   ext4_dir_nlink, 
@@ -250,6 +252,7 @@ rescue RuntimeError => e
 end
 
 puts "mounting test disk"
+STDOUT.flush
 mount_opts = "-v -t ext4 -o "
 if ext4_barrier == "barrier"
   mount_opts += "barrier=1"
@@ -290,14 +293,17 @@ end
 puts "removing previous test results"
 run_program(vm, guestauth, "/bin/bash", "-c 'rm -rf /home/dan/.phoronix-test-suite/test-results/*'")
 
-puts "running phoronix test suite"
+puts "#{DateTime.now}: running phoronix test suite"
+STDOUT.flush
+
 twelve_hours_in_secs = 12 * 60 * 60
 
 #benchmark = "pts/aio-stress"
 #benchmark = "pts/linux-system" ???
 benchmark = "pts/disk"
 run_shell_capture_output(vm, guestauth, "cd /home/dan/phoronix-test-suite && RUN_TESTS_IN_RANDOM_ORDER=yes bash /home/dan/benchmark.sh #{benchmark}", twelve_hours_in_secs)
-puts "phoronix test suite complete"
+puts "#{DateTime.now} phoronix test suite complete"
+STDOUT.flush
 
 puts "copying back results"
 FileUtils.rm_f %w(benchmark-results.xml)
